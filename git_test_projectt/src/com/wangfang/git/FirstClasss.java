f7e4b112f4b19a918cd206967e676b0201649e1a
wangfang <13588830933@163.com>
2019-06-05 16:54:36 +0800

    把本地的工程添加到git

---
+++ git_test_projectt/src/com/wangfang/git/FirstClasss.java
@@ -0,0 +1,12 @@
+package com.wangfang.git;
+
+public class FirstClasss {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
